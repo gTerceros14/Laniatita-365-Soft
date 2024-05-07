398387adb8c29c27d0940f392b8325ac03f80887
German Terceros <german.terceros@uab.edu.bo>
2024-05-07 10:10:59 -0400

    cambiando foto en cuenta

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -31,7 +31,7 @@
             <notification :notifications="notifications"></notification>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                    <img src="img/Logo-Lananita.png" class="img-avatar" alt="Fotografia">
+                    <img src="img/Logo-Lananita.jpg" class="img-avatar" alt="Fotografia">
                     <span class="d-md-down-none">{{Auth::user()->usuario}}</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
